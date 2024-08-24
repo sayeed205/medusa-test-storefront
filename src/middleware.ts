52fae190618792f95092d84d954341eba198df5a
Hitarashi <sayeed205@gmail.com>
2024-08-24 23:27:38 +0530

    backend url log

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,6 +18,7 @@ async function getRegionMap() {
     regionMapUpdated < Date.now() - 3600 * 1000
   ) {
     // Fetch regions from Medusa. We can't use the JS client here because middleware is running on Edge and the client needs a Node environment.
+    console.log("BACKEND_URL: ", BACKEND_URL)
     const { regions } = await fetch(`${BACKEND_URL}/store/regions`, {
       next: {
         revalidate: 3600,
